ad2afd35b67bd09045f4d086d3de1bdf100dc150
Diamond Lewis <findlewis@gmail.com>
2024-07-08 17:52:20 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,10 @@
-export function booleanParser(opt) {
+function booleanParser(opt) {
   if (opt == true || opt == 'true' || opt == '1') {
     return true;
   }
   return false;
+}
+
+export {
+  booleanParser,
 }
