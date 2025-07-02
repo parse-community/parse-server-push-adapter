22bd46ab93a5a0975059225407b4e628866c4300
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2025-07-03 00:08:19 +0200

    increase timeout

--- spec/helper.js
+++ spec/helper.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 global.__dirname = __dirname;
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.PARSE_SERVER_TEST_TIMEOUT || 5000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = process.env.PARSE_SERVER_TEST_TIMEOUT || 10_000;
 
 jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(new SpecReporter());
